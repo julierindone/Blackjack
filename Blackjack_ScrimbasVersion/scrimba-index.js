b62e142a5b0fcd4c66282a07ed2cd46d5e1f6241
Julie Rindone <julie.rindone@gmail.com>
2023-09-02 21:46:41 -0500

    Added Scrimba versions of index.js & index.html before adding array

---
+++ Blackjack_ScrimbasVersion/scrimba-index.js
@@ -0,0 +1,44 @@
+// BLACKJACK - SCRIMBA VERSION
+
+// Although we haven't been 100% on the same page up through the point of ,
+// my version veered further away when it came to displaying cards after the first 2. 
+//I solved the problem of listing by separating out the cards and total
+// by putting them in a different method, while Per is creating an array.
+// I'm not sure either is better, but maybe the array way will be more reusable. 
+let firstCard = 10
+let secondCard = 4
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+function startGame() {
+    renderGame()
+}
+
+function renderGame() {
+    // render out firstCard and secondCard
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    // render out ALL the cards we have
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+
+function newCard() {
+    let card = 6
+    sum += card
+    renderGame()
+}
