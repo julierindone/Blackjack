e6aa2c5c02d728b55d6726dcf7c17dd178268482
Julie Rindone <julie.rindone@gmail.com>
2023-09-05 18:38:52 -0500

    Moved "Cards"  text  from index.js to index.html

--- Blackjack_ScrimbasVersion/scrimba-index-D_River.js
+++ Blackjack_ScrimbasVersion/scrimba-index-D_River.js
@@ -1,22 +1,21 @@
 // BLACKJACK - SCRIMBA VERSION
 
+// Both versions have gone off the rails at this point. 
 let firstCard = 3
 let secondCard = 8
-
 let cards = [firstCard, secondCard]
-let cardsEl = document.getElementById("cards-el")
-
 let total = 0
-let totalEl = document.getElementById("total-el")
-
-let message = ""
-let messageEl = document.getElementById("message-el")
-
 let hasBlackJack = false
 let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let totalEl = document.getElementById("total-el")
+let cardsEl = document.getElementById("cards-el")
 
 function startGame() {
   total = 0
+  cards = [firstCard, secondCard]
+  // cardString = ""
   totalEl.textContent = total
   renderGame(cards)
   score()
@@ -31,6 +30,8 @@ function renderGame(cards) {
   }
   cardsEl.textContent = cardString
   totalEl.textContent = `Total: ${total}`
+
+  // score()
 }
 
 function score() {
@@ -41,20 +42,27 @@ function score() {
   else if (total == 21) {
     message = "You've got Blackjack!"
     hasBlackJack = true
+    isAlive= false
+    total = 0
+
   }
   else {
     message = "You're out of the game!"
     isAlive = false
+    total = 0
   }
   messageEl.textContent = message
 }
 
 function newCard() {
+  console.log("isAliveinside newCard() " + isAlive)
+
   if (isAlive === false) {
     messageEl.textContent = "CLICK START TO PLAY AGAIN"
   }
   
   else {
+  // if (isAlive=== true) {
     let card = 6
     cards.push(card)
     total += card
@@ -64,8 +72,4 @@ function newCard() {
 
     score()
   }
-  // else {
-    // message.textContent = "CLICK START TO PLAY AGAIN"
-  // }
-
 }
